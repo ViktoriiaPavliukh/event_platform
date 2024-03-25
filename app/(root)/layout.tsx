30fdac59898be0239ca621407b2c57ba9b65ee3d
ViktoriiaPavliukh <victoriataran.mail@gmail.com>
2024-03-25 12:19:19 +0000

    install dependencies, set up basic structure of components

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,16 @@
+import Footer from "@/components/ui/shared/Footer";
+import { Header } from "@/components/ui/shared/Header";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </div>
+  );
+}
---
+++ app/(root)/page.tsx
@@ -0,0 +1,10 @@
+import ButtonContained from "@/components/ui/ButtonContained";
+import styles from "./../page.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <ButtonContained />
+    </main>
+  );
+}
