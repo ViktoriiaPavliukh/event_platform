b3719ee3a7d33eaa6c175445ff9f79f4190753d0
ViktoriiaPavliukh <victoriataran.mail@gmail.com>
2024-04-02 00:05:21 +0300

    create collections and update cards

--- components/shared/Collections.tsx
+++ components/shared/Collections.tsx
@@ -1,6 +1,8 @@
 "use client";
 import React from "react";
 import { IEvent } from "@/lib/database/models/event.model";
+import { Box, Typography, List, ListItem, ListItemText } from "@mui/material";
+import Card from "./Card";
 
 type CollectionProps = {
   data: IEvent[];
@@ -22,7 +24,56 @@ const Collections = ({
   collectionType,
   urlParamName,
 }: CollectionProps) => {
-  return <div>Collections</div>;
+  return (
+    <>
+      {data.length > 0 ? (
+        <Box>
+          {" "}
+          <List component="ul">
+            {data.map((event) => {
+              const hasOrderLink = collectionType === "Events_Organised";
+              const hidePrice = collectionType === "My_tickets";
+
+              return (
+                <ListItem component="li" key={event._id}>
+                  <Card
+                    event={event}
+                    hasOrderLink={hasOrderLink}
+                    hidePrice={hidePrice}
+                  />
+                </ListItem>
+              );
+            })}
+            {/* <ListItem component="li">
+              <ListItemText primary="Item 1" />
+            </ListItem>
+            <ListItem component="li">
+              <ListItemText primary="Item 2" />
+            </ListItem>
+            <ListItem component="li">
+              <ListItemText primary="Item 3" />
+            </ListItem> */}
+          </List>
+        </Box>
+      ) : (
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            gap: "20px",
+            padding: "50px",
+            borderRadius: "8px",
+            backgroundColor: "#f8f4f0",
+          }}
+        >
+          <Typography variant="h5">{emptyTitle}</Typography>
+          <Typography variant="body1">{emptyStateSubtext}</Typography>
+        </Box>
+      )}
+    </>
+  );
 };
 
 export default Collections;
---
+++ components/shared/DeleteModal.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import { useState } from "react";
+import { usePathname } from "next/navigation";
+import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from "@mui/material";
+import { deleteEvent } from "@/lib/actions/event.actions";
+
+const DeleteModal = ({ eventId }: { eventId: string }) => {
+  const [open, setOpen] = useState(false);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const pathname = usePathname();
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleDelete = async () => {
+    setIsDeleting(true);
+    await deleteEvent({ eventId, path: pathname });
+    setIsDeleting(false);
+    setOpen(false);
+  };
+
+  return (
+    <>
+      <DeleteOutlineIcon onClick={handleClickOpen} />
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Are you sure you want to delete?</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            This will permanently delete this event.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary" disabled={isDeleting}>
+            Cancel
+          </Button>
+          <Button
+            onClick={handleDelete}
+            color="primary"
+            autoFocus
+            disabled={isDeleting}
+          >
+            {isDeleting ? "Deleting..." : "Delete"}
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
+};
+
+export default DeleteModal;
